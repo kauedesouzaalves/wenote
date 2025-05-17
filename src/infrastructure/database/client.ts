4300bb6eeb38793baaea25a99a81e201f1ceaa26
kauedesouzaalves <kkauedsouza.alves@gmail.com>
2025-05-17 04:50:45 -0300

    refactor: streamline database connection

--- src/infrastructure/database/client.ts
+++ src/infrastructure/database/client.ts
@@ -3,11 +3,7 @@ import "dotenv/config";
 import { Pool } from "pg";
 
 const db = new Pool({
-    user: process.env.POSTGRE_USER,
-    host: process.env.POSTGRE_HOST,
-    database: process.env.POSTGRE_DB,
-    password: process.env.POSTGRE_PASSWORD,
-    port: Number(process.env.POSTGRE_PORT),
+    connectionString: process.env.DATABASE_URL,
 });
 
 export default db;
