728520a83fe13a827fb2075bf62756590c6968cc
kauedesouzaalves <kkauedsouza.alves@gmail.com>
2025-05-26 02:56:34 -0300

    feat: add user DTO

--- src/use-cases/UserUseCase.ts
+++ src/use-cases/UserUseCase.ts
@@ -1,12 +1,40 @@
 import { IUserEntity } from "@/entities/UserEntity";
 import { IPostgreUserRepository } from "@/repositories/PostgreUserRepository";
+import UserDTO from "./dtos/UserDTO";
 
-export interface IUserUseCase {}
+export interface IUserUseCase {
+    createUser(
+        userProps: Pick<
+            IUserEntity,
+            "displayName" | "username" | "email" | "password"
+        >
+    ): Promise<IUserEntity>;
+    // getUserById(id: string): Promise<IUserEntity>;
+}
 
 class UserUseCase implements IUserUseCase {
     constructor(private userRepository: IPostgreUserRepository) {}
 
-    createUser(userProps: IUserEntity) {}
+    async createUser(
+        userProps: Pick<
+            IUserEntity,
+            "displayName" | "username" | "email" | "password"
+        >
+    ): Promise<IUserEntity> {
+        const userData = await this.userRepository.createUser(userProps);
+        const newUser = new UserDTO(userData).toEntity();
+        return newUser;
+    }
+
+    // async getUserById(id: string): Promise<IUserEntity> {
+    //     const user = await this.userRepository.getUserById(id);
+
+    //     // Hydrate object
+
+    //     UserDTO();
+
+    //     return user;
+    // }
 }
 
 export default UserUseCase;
