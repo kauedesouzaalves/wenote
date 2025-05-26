229ac35a828723d847f65a7d72b8ee169029dc4e
kauedesouzaalves <kkauedsouza.alves@gmail.com>
2025-05-26 03:56:10 -0300

    style: change some elements style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import "@/styles/index.css";
 export default function Home() {
     return (
         <>
-            <section className="hero-section pt-15">
+            <section className="hero-section mt-15">
                 <div className="max-w-[1024px] mx-auto">
                     <div>
                         <span className="hero-title text-5xl font-bold">
@@ -14,7 +14,7 @@ export default function Home() {
                     </div>
                     <br />
                     <div>
-                        <span className="hero-subtitle text-1xl font-bold">
+                        <span className="hero-subtitle font-bold">
                             Crie, compartilhe e edite anotações de forma simples
                             e colaborativa. <br />
                             Tudo em tempo real, com uma experiência fluida e
