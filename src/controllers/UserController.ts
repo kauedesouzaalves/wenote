902457aebd39c7b4ef0220276d7b21a51ec97eef
kauedesouzaalves <kkauedsouza.alves@gmail.com>
2025-05-26 03:55:22 -0300

    feat: implement registerUser method and UserCredentials interface

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,7 +1,27 @@
+import { IUserEntity } from "@/entities/UserEntity";
 import { IUserUseCase } from "@/use-cases/UserUseCase";
 
-class UserController {
+interface UserCredentials {
+    displayName: string;
+    username: string;
+    email: string;
+    password: string;
+}
+
+export interface IUserController {
+    registerUser(userCredentials: UserCredentials): Promise<IUserEntity>;
+    getUserById(): void;
+}
+
+class UserController implements IUserController {
     constructor(private userUseCase: IUserUseCase) {}
+
+    async registerUser(userCredentials: UserCredentials): Promise<IUserEntity> {
+        const newUser = await this.userUseCase.createUser(userCredentials);
+        return newUser;
+    }
+
+    getUserById() {}
 }
 
 export default UserController;
