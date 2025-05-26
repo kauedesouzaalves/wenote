27a2f770b54afab0cba785d7995d7b56ff0ccf88
kauedesouzaalves <kkauedsouza.alves@gmail.com>
2025-05-26 03:56:49 -0300

    feat: create register page

--- src/app/register/layout.tsx
+++ src/app/register/layout.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: "WeNote | Register",
+    title: "WeNote | Registrar",
     description:
         "Crie sua conta gratuita na WeNote e comece a organizar suas ideias de forma colaborativa. Junte-se a uma plataforma moderna de anotações em tempo real e eleve sua produtividade.",
 };
